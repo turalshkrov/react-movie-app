cd51aa0866304be721d746b45db59abc7fefb903
Tural Şükürov <tural.shkrov@gmail.com>
2023-01-25 15:56:41 +0400

    final commit 3

--- src/components/components/footer/footer.js
+++ src/components/components/footer/footer.js
@@ -5,10 +5,10 @@ export default function Footer() {
   return (
     <div className='footer'>
         <div className='api-link'>
-            <a href='https://www.omdbapi.com/' target='_blank'>OMDb api</a>
+            <a href='https://www.omdbapi.com/' target='_blank' rel="noopener noreferrer">OMDb api</a>
         </div>
         <div className='github-link'>
-        <a href='https://github.com/turalshkrov' target='_blank'>Github</a>
+        <a href='https://github.com/turalshkrov' target='_blank' rel="noopener noreferrer">Github</a>
         </div>
     </div>
   )
